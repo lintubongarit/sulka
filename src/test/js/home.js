de103696f3e54c34d8bc3cc22ac424c82fd47400
Ville Sinisalo <vesinisa@cs.reducethis.helsinki.fi>
2013-09-16 14:02:33 +0300

    merge from master

--- src/test/js/home.js
+++ src/test/js/home.js
@@ -4,18 +4,18 @@ casper.test.begin('Home page tests', 7, function suite(test) {
     browse('/', function () {
         test.assertHttpStatus(200, "HTTP status is OK");
         test.assertTitle("Home", "Title is 'Home'");
-        test.assertSelectorHasText('body > h1', "Hello world!", "Has a header containing the greeting");
+        test.assertSelectorHasText('body > h1', "Hello world!", "Has a header containing the greeting 'Hello world!'");
         
         var now = moment();
-        var time = now.format("HH:mm");
+        var time = now.format(":mm");
         var dayOfMonth = now.format("D");
         var year = now.format("YYYY");
         var dateText = casper.evaluate(function () { return __utils__.findOne('body > p').textContent });
     
-        test.assert(dateText.indexOf("The time on the server is ") >= 0, "Page has timestamp");
-        test.assert(dateText.indexOf(time) >= 0, "Timestamp has correct time");
-        test.assert(dateText.indexOf(dayOfMonth) >= 0, "Timestamp has correct day of month");
-        test.assert(dateText.indexOf(year) >= 0, "Timestamp has correct year");
+        test.assertSelectorHasText("body > p", "The time on the server is ", "Page has timestamp");
+        test.assertSelectorHasText("body > p", time, "Timestamp has correct time");
+        test.assertSelectorHasText("body > p", dayOfMonth, "Timestamp has correct day of month");
+        test.assertSelectorHasText("body > p", year, "Timestamp has correct year");
     });
     
     casper.run(function () {
--- src/test/js/run-casperjs-tests.sh
+++ src/test/js/run-casperjs-tests.sh
@@ -1,4 +1,17 @@
-#!/bin/sh
-# Install CasperJS 1.1.0 to $HOME/bin and launch the site to http://localhost:8080/ to run frontend tests
+#!/bin/bash
+# Install latest CasperJS and PhantomJS to $HOME/bin or globally and launch the site to http://localhost:8080/ to run the frontend tests
+# Alternatively specify PATH to casperjs and phantomjs as $1 and site URL as $2
+
 export PATH=$PATH:$HOME/bin
-casperjs test --includes=lib/default_base_url.js,lib/test_helpers.js --xunit=../../../target/surefire-reoprts/casperjs.xml --no-colors *.js
+if [ -n "$1" ]; then
+	export PATH=$1:$PATH
+fi
+if [ -n "$2" ]; then
+	trap '[ -e "$tmp_file" ] && rm "$tmp_file"' EXIT
+	tmp_file=`mktemp --tmpdir casperjs_url.XXXXX.js`
+	echo 'const URL="'$2'";' > $tmp_file
+	url_include=",$tmp_file"
+else
+	url_include=""
+fi
+casperjs test --includes=lib/default_base_url.js,lib/test_helpers.js$url_include --xunit=../../../target/surefire-reports/casperjs.xml --no-colors *.js
