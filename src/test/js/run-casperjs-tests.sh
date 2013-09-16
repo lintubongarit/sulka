929b00846c42e8dbe234a36d8741c20716db3dd1
Ville Sinisalo <vesinisa@cs.reducethis.helsinki.fi>
2013-09-16 10:12:45 +0300

    add casperjs test target to maven (exec:exec)

--- src/test/js/run-casperjs-tests.sh
+++ src/test/js/run-casperjs-tests.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-# Install CasperJS 1.1.0 and launch the site to http://localhost:8080/ to run frontend tests
-casperjs test --includes=lib/default_base_url.js,lib/test_helpers.js *.js
+# Install CasperJS 1.1.0 to $HOME/bin and launch the site to http://localhost:8080/ to run frontend tests
+export PATH=$PATH:$HOME/bin
+casperjs test --includes=lib/default_base_url.js,lib/test_helpers.js --xunit=../../../target/surefire-reoprts/casperjs.xml --no-colors *.js
