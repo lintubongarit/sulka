64f5a39285d764dfbc62c68fd51a71f3b5fb31d4
“Olli <olli.raitio@helsinki.fi>
2013-09-16 15:44:28 +0300

    yhteys tipuAPIIn, lajien hakeminen ja nayttaminen

--- src/main/java/edu/helsinki/sulka/HomeController.java
+++ src/main/java/edu/helsinki/sulka/HomeController.java
@@ -10,30 +10,103 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.apache.commons.codec.binary.Base64;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.HttpContext;
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.stereotype.Component;
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+
+	public class ContextAwareHttpComponentsClientHttpRequestFactory extends
+	HttpComponentsClientHttpRequestFactory {
+		private HttpContext httpContext;
+
+		public ContextAwareHttpComponentsClientHttpRequestFactory(HttpClient httpClient){
+			super(httpClient);
+		}
+
+		protected HttpContext createHttpContext(HttpMethod httpMethod, URI uri) {
+			//Ignoring the URI and method.
+			return httpContext;
+		}
+
+		public void setHttpContext(HttpContext httpContext) {
+			this.httpContext = httpContext;
+		}
+	}
+
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+	public String home(Locale locale, Model model) throws Exception {
+
+		URL url = new URL("https://h92.it.helsinki.fi/tipu-api/species?format=json");
+
+		String name = "bongarit-dev";
+		String password = "devbongarit";
+
+		String authString = name + ":" + password;
+		byte[] authEncBytes = Base64.encodeBase64(authString.getBytes());
+		String authStringEnc = new String(authEncBytes);
+
+		URLConnection conn = url.openConnection();
+		conn.setRequestProperty("Authorization", "Basic " + authStringEnc);
+
+		BufferedReader br = new BufferedReader(
+        new InputStreamReader(conn.getInputStream()));
+
+		String json = "";
+		String inputLine = "";
+
+		while (true) {
+			inputLine = br.readLine();
+			if (inputLine == null)
+				break;
+			json += inputLine + "\n";
+		}
+		json = json.substring(22, json.length() - 3);
+		model.addAttribute("json", json);
+
+  		ObjectMapper mapper = new ObjectMapper();
+
+		Species species[] = mapper.readValue(json, Species[].class);
+
+		model.addAttribute("json", species);
+
 		return "home";
 	}
-	
+
 }
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -1,14 +1,24 @@
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<%@ page session="false" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ page session="false"%>
 <html>
 <head>
-	<title>Home</title>
+<title>Home</title>
 </head>
 <body>
-<h1>
-	Hello world!  
-</h1>
-
-<P>  The time on the server is ${serverTime}. </P>
+	<h1>Hello world!</h1>
+	${json}
+	<table border=1>
+		<c:forEach items="${json}" var="species">
+			<tr>
+				<td><b>${species.getId()}</b></td>
+				<c:forEach items="${species.getName()}" var="name">
+					<td>content: ${name.getContent()}</td>
+					<td>Lang: ${name.getLang()}</td>
+				</c:forEach>
+				<td>${species.getEuringCode()}</td>
+				<td>${species.getProtectionStatus()}</td>
+			</tr>
+		</c:forEach>
+	</table>
 </body>
 </html>
