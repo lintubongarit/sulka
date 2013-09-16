248d1e68fe7260c3ee586fa22442051eb4f19d6d
Ville Sinisalo <vesinisa@cs.reducethis.helsinki.fi>
2013-09-16 10:18:33 +0300

    fix typo

--- src/test/js/run-casperjs-tests.sh
+++ src/test/js/run-casperjs-tests.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Install CasperJS 1.1.0 to $HOME/bin and launch the site to http://localhost:8080/ to run frontend tests
 export PATH=$PATH:$HOME/bin
-casperjs test --includes=lib/default_base_url.js,lib/test_helpers.js --xunit=../../../target/surefire-reoprts/casperjs.xml --no-colors *.js
+casperjs test --includes=lib/default_base_url.js,lib/test_helpers.js --xunit=../../../target/surefire-reports/casperjs.xml --no-colors *.js
