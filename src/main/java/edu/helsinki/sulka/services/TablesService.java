69794dd5e2d281eb86b36444631aea5acf4f1d61
Heikki Kalliokoski <heikki.kalliokoski@cs.helsinki.fi>
2013-09-25 11:59:01 +0300

    Set TablesService return empty table if queried w/o parameters
    Calling getTable() -method without parameters returns empty table.
    Added test to confirm that.

--- src/main/java/edu/helsinki/sulka/services/TablesService.java
+++ src/main/java/edu/helsinki/sulka/services/TablesService.java
@@ -1,5 +1,11 @@
 package edu.helsinki.sulka.services;
 
+import edu.helsinki.sulka.models.Table;
+
 public class TablesService {
+
+	public Table getTable() {
+		return new Table();
+	}
 
 }
--- src/test/java/edu/helsinki/sulka/services/TablesServiceTest.java
+++ src/test/java/edu/helsinki/sulka/services/TablesServiceTest.java
@@ -2,14 +2,23 @@ package edu.helsinki.sulka.services;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import edu.helsinki.sulka.models.Table;
 
 public class TablesServiceTest {
 
 	private TablesService tablesService;
 	
+	@Before
 	public void setUp(){
 		this.tablesService = new TablesService();
+	}
+
+	@Test
+	public void testGetTableWithNoParametersReturnsEmptyTable(){
+		assertTrue(this.tablesService.getTable().getRows().isEmpty());
 	}
 
 }
