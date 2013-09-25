775ffec6d5494af9a729e0c50b32ffd746d6b68c
Heikki Kalliokoski <heikki.kalliokoski@cs.helsinki.fi>
2013-09-25 10:44:08 +0300

    Initialize tables rows with empty list
    Set Table-classes constructor to initialize rows-variable
    with empty ArrayList. Add test to verify that.

--- src/main/java/edu/helsinki/sulka/models/Table.java
+++ src/main/java/edu/helsinki/sulka/models/Table.java
@@ -1,5 +1,18 @@
 package edu.helsinki.sulka.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Table {
+
+	private List<Row> rows;
+	
+	public Table(){
+		this.rows = new ArrayList<Row>();
+	}
+	
+	public List<Row> getRows() {
+		return this.rows;
+	}
 
 }
--- src/test/java/edu/helsinki/sulka/models/TableTest.java
+++ src/test/java/edu/helsinki/sulka/models/TableTest.java
@@ -2,10 +2,21 @@ package edu.helsinki.sulka.models;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TableTest {
 
+	private Table table;
 	
+	@Before
+	public void setUp(){
+		this.table = new Table();
+	}
 
+	@Test
+	public void testThereAreNoRowsAfterInit(){
+		assertTrue(this.table.getRows().isEmpty());
+	}
+	
 }
