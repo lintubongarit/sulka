9333e6b1633fbb7f1a7179afb2eb3dd3379f1f35
Henriikka <henriikka.palva@helsinki.fi>
2013-10-07 13:22:05 +0300

    add some basic test for ui

---
+++ src/test/js/ui.js
@@ -0,0 +1,16 @@
+var jquery = require('../../main/webapp/resources/js/jquery-1.7.min.js');
+var jqueryEvent = require('../../main/webapp/resources/js/jquery.event.drag-2.2.js');
+var slickCore = require('../../main/webapp/resources/js/slick.core.js');
+var slickGrid = require('../../main/webapp/resources/js/slick.grid.js');
+
+casper.test.begin('Uitests', 4, function suite(test) {
+	casper.options.timeout = 600000;
+    	casper.start('http://localhost:8080/sulka/slick', function () {
+        	test.assertHttpStatus(200, "HTTP status is OK");
+        	test.assertTitle("Sulka", "Title is 'Sulka'");
+    	});
+	
+    	casper.run(function () {
+        	test.done();
+   	});
+});
