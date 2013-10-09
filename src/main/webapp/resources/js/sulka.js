604791bfda098260944f4130fe6790a33901bb22
Heikki Kalliokoski <heikki.kalliokoski@cs.helsinki.fi>
2013-10-09 15:27:25 +0300

    Change fetchFields to use .ajax instead of .getJSON
    	When using .getJSON the code didn't stop execution
    	to wait fetching to finish and that caused usage of
    	uninitialized variables. With .ajax execution stops
    	and waits to query finish.

--- src/main/webapp/resources/js/sulka.js
+++ src/main/webapp/resources/js/sulka.js
@@ -1,18 +1,23 @@
 var sulka = {
 	fetchFields: function (){
 		var fetchedFields = new Array();
-		$.getJSON("api/fields/groups", function(json){
-			$.each(json.objects, function(index, fieldGroup){
-				fieldsInGroup = fieldGroup['fields'];
-				$.each(fieldsInGroup, function(indexB, field){
-					var columnHeader = {
-						id: field['field'],
-						name: field['name'],
-						field: field['field']
-					};
-					fetchedFields.push(columnHeader);
+		$.ajax({
+			url: "api/fields/groups",
+			async: false,
+			dataType: 'json',
+			success: function(result){
+				$.each(result.objects, function(index, fieldGroup){
+					fieldsInGroup = fieldGroup['fields'];
+					$.each(fieldsInGroup, function(indexB, field){
+						var columnHeader = {
+							id: field['field'],
+							name: field['name'],
+							field: field['field']
+						};
+						fetchedFields.push(columnHeader);
+					});
 				});
-			});
+			}
 		});
 		return fetchedFields;
 	}
