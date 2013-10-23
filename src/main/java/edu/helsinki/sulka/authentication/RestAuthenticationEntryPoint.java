302e4dd520fffa06a4aed4f3904d3f3032f3553e
Heikki Holopainen <nospamhessuh@gmail.com>
2013-10-23 14:19:26 +0300

    Custom HTTP status pages

--- src/main/java/edu/helsinki/sulka/authentication/RestAuthenticationEntryPoint.java
+++ src/main/java/edu/helsinki/sulka/authentication/RestAuthenticationEntryPoint.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException e) throws IOException, ServletException {
-        response.sendError( HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized" );
+        //response.sendError( HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+    	response.sendError( HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
     }
 }
