c973c951612eb306d32a2634c8ddeba338a1d569
“Olli <olli.raitio@helsinki.fi>
2013-10-25 15:34:30 +0300

    forgot to include new files

---
+++ src/main/webapp/resources/js/sulka.addRecovery.js
@@ -0,0 +1,25 @@
+sulka.addRecovery = function(addRecovery) {
+
+	sulka.viewMode = "ringings";
+	
+	sulka.getRowMode = function() {
+		sulka.rowsMode = "ringings";
+	};
+
+	sulka.addRow = function() {
+		var data = sulka.grid.getData();
+		data.unshift({});
+		sulka.grid.setData(data);
+		sulka.grid.render();
+	};
+
+	sulka.getDate = function() {
+		var now = new Date();
+		var date = now.getDate() + '.' + (now.getMonth() + 1) + '.'
+				+ (now.getFullYear() - 11) + '-' + now.getDate() + '.'
+				+ (now.getMonth() + 1) + '.' + now.getFullYear();
+		console.log(date);
+		return date;
+	};
+
+}();
