dd9dc7521174e72d7359d315f0f5296df6351227
Heikki Kalliokoski <heikki.kalliokoski@cs.helsinki.fi>
2013-10-28 11:56:43 +0200

    Add default name for ringings table

--- src/main/java/edu/helsinki/sulka/models/DbRowRingings.java
+++ src/main/java/edu/helsinki/sulka/models/DbRowRingings.java
@@ -7,8 +7,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="Ringings")
 public class DbRowRingings implements Serializable {
 	
 	@Id
