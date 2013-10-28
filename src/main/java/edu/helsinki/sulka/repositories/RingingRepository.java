4007b8b2aaa5be496b24edb99d8c2b6ca243ac6d
Heikki Kalliokoski <heikki.kalliokoski@cs.helsinki.fi>
2013-10-28 14:48:30 +0200

    Add findByUserId -query to RingingsRepository

--- src/main/java/edu/helsinki/sulka/repositories/RingingRepository.java
+++ src/main/java/edu/helsinki/sulka/repositories/RingingRepository.java
@@ -1,9 +1,12 @@
 package edu.helsinki.sulka.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import edu.helsinki.sulka.models.DbRowRingings;
 
 
 public interface RingingRepository extends CrudRepository<DbRowRingings, Long>{
+	List<DbRowRingings> findByUserId(Integer userId);
 }
