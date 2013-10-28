cbde5cada0873d9ac623fb188b60f9a7fe4a1382
Heikki Kalliokoski <heikki.kalliokoski@cs.helsinki.fi>
2013-10-28 14:05:36 +0200

    Update DbRowRingings

--- src/main/java/edu/helsinki/sulka/models/DbRowRingings.java
+++ src/main/java/edu/helsinki/sulka/models/DbRowRingings.java
@@ -13,39 +13,27 @@ import javax.persistence.Table;
 @Table(name="Ringings")
 public class DbRowRingings implements Serializable {
 	
+	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	@Column(name = "id")
 	private Long id;
-	
-	public DbRowRingings(){
-	}
-	
-	public DbRowRingings(Long id, String row){
-		this.id = id;
-		this.row = row;
-	}
+	@Column(name = "row")
+	private String row;
 	
 	public Long getId() {
 		return id;
 	}
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
-	@Column(name = "row")
-	private String row;
-
 	public String getRow() {
 		return row;
 	}
-
 	public void setRow(String row) {
 		this.row = row;
 	}
-	
-	
-	
-
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
 }
