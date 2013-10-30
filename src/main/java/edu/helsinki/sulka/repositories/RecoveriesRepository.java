ad6f5f2a9fc41ea3497b948d5255c5fe0032e368
Heikki Kalliokoski <heikki.kalliokoski@cs.helsinki.fi>
2013-10-30 09:58:14 +0200

    Add RecoveriesRepository -class and testclass

---
+++ src/main/java/edu/helsinki/sulka/repositories/RecoveriesRepository.java
@@ -0,0 +1,11 @@
+package edu.helsinki.sulka.repositories;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import edu.helsinki.sulka.models.DatabaseRow;
+
+public interface RecoveriesRepository extends CrudRepository<DatabaseRow, Long>{
+	List<DatabaseRow> findByUserId(String userId);
+}
