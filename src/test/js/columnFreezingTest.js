9b9375617460275eb3837c32276f4d83134796c5
Ville Sinisalo <vesinisa@cs.reducethis.helsinki.fi>
2013-11-06 14:27:48 +0200

    add column freezing tests

---
+++ src/test/js/columnFreezingTest.js
@@ -0,0 +1,41 @@
+casper.test.begin('Column freezing tests', 10, function suite(test) {
+    browse('/', function () {
+    	var firstColumn = null,
+    		firstFreezeGridColumn = null;
+    	
+    	casper
+			.then(function () {
+				this.click('#browse-tab');
+			})
+	    	.waitWhileVisible("#loader-animation")
+			.then(function () {
+				test.assertVisible("#slick-grid .header-freeze-button", "Freeze button is visible");
+				test.assertNotVisible("#freeze-grid", "Freeze grid is not visible");
+				firstColumn = get('sulka.grid.getColumns()[0].id');
+				
+				get('$("#slick-grid .header-freeze-button").click()');
+			})
+			.then(function () {
+				test.assertVisible("#slick-grid .header-freeze-button", "Freeze button is visible");
+				test.assertVisible("#freeze-grid .header-unfreeze-button", "Unfreeze button is visible");
+				
+				firstFreezeGridColumn = get('sulka.freeze.grid.getColumns()[0].id');
+				test.assertEquals(firstFreezeGridColumn, firstColumn, "First column is now in freeze grid");
+				firstColumn = get('sulka.grid.getColumns()[0].id');
+				test.assertNotEquals(firstColumn, firstFreezeGridColumn, "First column is not anymore in main grid");
+				
+				get('$("#freeze-grid .header-unfreeze-button").click()');
+			})
+			.then(function () {
+				test.assertVisible("#slick-grid .header-freeze-button", "Freeze button is visible");
+				test.assertNotVisible("#freeze-grid", "Freeze grid is not visible");
+				
+				test.assertEquals(get('sulka.grid.getColumns()[0].id'), firstFreezeGridColumn, "Frozen column is back in main grid");
+				test.assertEquals(get('sulka.grid.getColumns()[1].id'), firstColumn, "Previous first column has moved back");
+			});
+    });
+    
+    casper.run(function () {
+        test.done();
+   	});
+});
