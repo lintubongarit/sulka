6a2755d00b14dda283ab3cea9705b1065dfd2e9e
“Olli <olli.raitio@helsinki.fi>
2013-11-06 15:36:33 +0200

    removed unnecessary debugging

--- src/main/webapp/resources/js/sulka.core.js
+++ src/main/webapp/resources/js/sulka.core.js
@@ -525,12 +525,6 @@ sulka = {
 	        sulka.grid.updateRowCount();
 	        sulka.grid.render();
 	        
-	        console.log('args')
-	        console.log(args);
-	        
-	        console.log('item')
-	        console.log(item);
-	        
 	        sulka.addToSulkaDB(args);
 	},
 	
