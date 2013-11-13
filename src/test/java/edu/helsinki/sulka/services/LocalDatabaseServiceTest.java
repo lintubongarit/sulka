84daa4e305dfee4a6d737d656018fea6c2d2805b
Heikki Kalliokoski <heikki.kalliokoski@cs.helsinki.fi>
2013-11-13 09:16:54 +0200

    Remove unnecessary import

--- src/test/java/edu/helsinki/sulka/services/LocalDatabaseServiceTest.java
+++ src/test/java/edu/helsinki/sulka/services/LocalDatabaseServiceTest.java
@@ -4,8 +4,6 @@ import static org.junit.Assert.*;
 
 import java.util.List;
 
-import javax.persistence.PersistenceContext;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
