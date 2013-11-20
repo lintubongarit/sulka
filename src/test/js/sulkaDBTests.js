e7820881531359f609ccc98980f2f911c07f5b0b
“Olli <olli.raitio@helsinki.fi>
2013-11-20 13:53:58 +0200

    deleted unrelevant test

--- src/test/js/sulkaDBTests.js
+++ src/test/js/sulkaDBTests.js
@@ -43,52 +43,7 @@ casper.test.begin('Sulka-database tests', 2, function suite(test) {
 	    	test.assertNotEquals(newRow, -1,
 	    	"sulka.core.onAddNewRow adds new row with wanted data to slickgrid and DB, and sulka.API.fetchSulkaDBRows fetches rows");
 	   
-	    }).then(function () {
-	    	var rowToEdit = {};
-	    	
-	    	casper.evaluate(function(){
-	    	
-				rowToEdit = sulka.grid.getData().getItem(newRow);
-				
-				rowToEdit.municipalities = 'sdf';
-		        
-				var args = {};
-				
-				args.item = rowToEdit;
-				args.row = sulka.grid.getData().getLength() - 1;
-				
-				sulka.onCellChange(null, args);
-	    	});
-	    	
-		}).then(function() {
-			this.reload(function() {
-	        });
-	    }).waitWhileVisible("#loader-animation"
-	    ).then(function () {
-	    	
-	    	var slickDataLength = casper.evaluate(function () {
-				return sulka.grid.getData().getLength();
-	        });
-	    	
-	    	var editedRow = -1;
-	    	
-	    	for (var i = 0; i < slickDataLength; i++){
-				var row = casper.evaluate(function(i){
-					return sulka.grid.getData().getItem(i);
-				}, i);
-				if (row.species !== undefined){
-					if (row.species.indexOf(randomSpecies) != -1){
-						if (row.municipalities = 'sdf'){
-							editedRow = i;
-							break;
-						}
-					}
-				}
-			}
-	    	
-	    	test.assertNotEquals(editedRow, -1,
-	    	"sulka.core.onCellChange edits row in sulka-DB");
-		});
+	    });
 		
 	});
 	
