a81b74f6f3c8ab9fe8d43906b0bbb7e80390180b
Heikki Kalliokoski <heikki.kalliokoski@cs.helsinki.fi>
2013-12-02 13:12:24 +0200

    Move event handlers to separate module

--- src/main/webapp/resources/js/sulka.core.js
+++ src/main/webapp/resources/js/sulka.core.js
@@ -226,18 +226,18 @@ sulka = {
 			
 			//Row move, drag & drop features
 			
-            sulka.grid.onBeforeEditCell.subscribe(sulka.onBeforeEditCell);
-			sulka.moveRowsPlugin.onBeforeMoveRows.subscribe(sulka.onBeforeMoveRows); 	  
-			sulka.moveRowsPlugin.onMoveRows.subscribe(sulka.onMoveRows);
-			sulka.grid.onDragInit.subscribe(sulka.grid.onDragInit);
-			sulka.grid.onDragStart.subscribe(sulka.onDragStart);
-			sulka.grid.onDrag.subscribe(sulka.onDrag);
-			sulka.grid.onDragEnd.subscribe(sulka.onDragEnd);
+            sulka.grid.onBeforeEditCell.subscribe(sulka.events.onBeforeEditCell);
+			sulka.moveRowsPlugin.onBeforeMoveRows.subscribe(sulka.events.onBeforeMoveRows); 	  
+			sulka.moveRowsPlugin.onMoveRows.subscribe(sulka.events.onMoveRows);
+			sulka.grid.onDragInit.subscribe(sulka.events.onDragInit);
+			sulka.grid.onDragStart.subscribe(sulka.events.onDragStart);
+			sulka.grid.onDrag.subscribe(sulka.events.onDrag);
+			sulka.grid.onDragEnd.subscribe(sulka.events.onDragEnd);
 			
-			sulka.grid.onKeyDown.subscribe(sulka.makeOnKeyDown());
-			sulka.grid.onDblClick.subscribe(sulka.onDblClick);
+			sulka.grid.onKeyDown.subscribe(sulka.events.makeOnKeyDown());
+			sulka.grid.onDblClick.subscribe(sulka.events.onDblClick);
 			
-			sulka.grid.onActiveCellChanged.subscribe(sulka.onActiveCellChanged);
+			sulka.grid.onActiveCellChanged.subscribe(sulka.events.onActiveCellChanged);
 			
 			//Init drop events?
 			sulka.initDrop();
@@ -259,23 +259,19 @@ sulka = {
 		sulka.grid.onHeaderContextMenu.subscribe(sulka.showColumnHeaderContextMenu);
 		$("#header-context-menu li.context-menu-item").click(sulka.headerContextMenuItemClicked);
 		
-		sulka.copyManager.onPasteCells.subscribe(sulka.onPasteCells);
+		sulka.grid.onSort.subscribe(sulka.events.onGridSort);
 		
-		sulka.grid.onSort.subscribe(sulka.onGridSort);
+		sulka.grid.onCellChange.subscribe(sulka.events.onCellChange);
 		
-		sulka.grid.onAddNewRow.subscribe(sulka.onAddNewRow);
-		
-		sulka.grid.onCellChange.subscribe(sulka.onCellChange);
-		
-		sulka.grid.onColumnsResized.subscribe(sulka.updateWidthToSulkaColumns);
-		sulka.grid.onColumnsReordered.subscribe(sulka.updateOrderToSulkaColumns);
+		sulka.grid.onColumnsResized.subscribe(sulka.events.updateWidthToSulkaColumns);
+		sulka.grid.onColumnsReordered.subscribe(sulka.events.updateOrderToSulkaColumns);
 		
 		$(window).resize(sulka.resizeGrid);
 		sulka.resizeGrid();
 		
 		var $slickGrid = $("#slick-grid");
 		if (typeof($slickGrid.mousewheel) === "function") {
-			$slickGrid.mousewheel(sulka.onMouseWheel);
+			$slickGrid.mousewheel(sulka.events.onMouseWheel);
 		}
 		
 		sulka.reloadData();
@@ -362,23 +358,7 @@ sulka = {
 	},
 	
 	
-	/**
-	 * Parameters for mouse wheel events
-	 */
-	viewport: null,
-	MOUSE_WHEEL_ROW_HEIGHT: 25,
-	MOUSE_WHEEL_SCROLL_ROWS: 3,
-	
-	/**
-	 * Handle mouse wheel events.
-	 */
-	onMouseWheel: function (event, delta, deltaX, deltaY) {
-		event.preventDefault();
-		if (deltaY !== 0) {
-			sulka.viewport.scrollTop(Math.max(0, sulka.viewport.scrollTop() - 
-					deltaY*sulka.MOUSE_WHEEL_SCROLL_ROWS*sulka.MOUSE_WHEEL_ROW_HEIGHT));
-		}
-	},
+
 	
 	/**
 	 * Return currently visible columns (even those not currently on grid)
@@ -391,15 +371,6 @@ sulka = {
 			}
 		});
 		return visible;
-	},
-	
-	/**
-	 * Called by SlickGrid when the grid needs to be sorted. 
-	 */
-	onGridSort: function (event, args) {
-		sulka.freeze.removeSortMarkers();
-		sulka.sort(args);
-		sulka.colouriseCellsWithErrors(sulka.getData());
 	},
 	
 	/**
@@ -419,222 +390,6 @@ sulka = {
 		sulka.freeze.invalidate();
 	},
 	
-	
-	/**
-	 *  Called before MoveRows
-	 */
-	onBeforeMoveRows: function(e,data) {
-	    for (var i = 0; i < data.rows.length; i++) {
-		    // no point in moving before or after itself
-		    if (data.rows[i] == data.insertBefore || data.rows[i] == data.insertBefore - 1) {
-			    e.stopPropagation();
-			    return false;
-		    }
-	    }
-	    return true;
-	},
-	
-	/**
-	 * OnMoveRows events
-	 */
-	onMoveRows: function(e,args) {
-		sulka.moveRowsPlugin.onMoveRows.subscribe(function (e, args) {
-		    var extractedRows = [], left, right;
-		    var rows = args.rows;
-		    var insertBefore = args.insertBefore;
-		    left = data.slice(0, insertBefore);
-		    right = data.slice(insertBefore, data.length);
-
-		    rows.sort(function(a,b) { return a-b; });
-
-		    for (var i = 0; i < rows.length; i++) {
-		      extractedRows.push(data[rows[i]]);
-		    }
-
-		    rows.reverse();
-
-		    for (var i = 0; i < rows.length; i++) {
-		      var row = rows[i];
-		      if (row < insertBefore) {
-		        left.splice(row, 1);
-		      } else {
-		        right.splice(row - insertBefore, 1);
-		      }
-		    }
-
-		    data = left.concat(extractedRows.concat(right));
-
-		    var selectedRows = [];
-		    for (var i = 0; i < rows.length; i++)
-		      selectedRows.push(left.length + i);
-
-		    sulka.grid.resetActiveCell();
-		    sulka.grid.setData(data);
-		    sulka.grid.setSelectedRows(selectedRows);
-		    sulka.grid.render();
-		  });
-		
-	},
-	
-	/**
-	 * onColumnResized updates column width changes to sulka.columns.
-	 */
-	updateWidthToSulkaColumns: function(e, args) {
-		var gridInd = 0;
-		var gridColumns = sulka.grid.getColumns();
-		for(var i = 0; i < sulka.columns.length; i++){
-			if(sulka.columns[i].$sulkaVisible && 
-				sulka.columns[i].field == gridColumns[gridInd].field){
-				sulka.columns[i].width = gridColumns[gridInd].width;
-				gridInd++;
-			}
-		}
-	},
-	
-	/**
-	 * onColumnsReorder updates order changes to sulka.columns.
-	 */
-	updateOrderToSulkaColumns: function(e, args) {
-		var columnIndex = 0;
-		var gridColumns = sulka.grid.getColumns();
-		var updatedColumnList = {};
-		for(var i = 0; i < sulka.columns.length; i++){
-			if(!sulka.columns[i].$sulkaVisible)
-				updatedColumnList[sulka.columns[i].field] = i;
-			else
-				if(columnIndex < gridColumns.length){
-					updatedColumnList[gridColumns[columnIndex].field] = i;
-					columnIndex++;
-				}
-		}
-		var newColumns = [];
-		for(var i = 0; i < sulka.columns.length; i++)
-			newColumns[updatedColumnList[sulka.columns[i].field]] = sulka.columns[i];
-		
-		sulka.columns = newColumns;
-	},
-	
-	
-	/**
-	 *	OnDragInit is used to prevent the grid from cancelling drag'n'drop by default 
-	 */
-	onDragInit: function(e,dd) {
-		sulka.grid.onDragInit.subscribe(function (e, dd) {
-		    // prevent the grid from cancelling drag'n'drop by default
-		    e.stopImmediatePropagation();
-		 });
-	},
-	
-	/**
-	 * Used to overwrite default edit navigation and editing. Returns an event handler function.
-	 */
-	makeOnKeyDown: function() {
-		var specialCodes = {};
-		for (var key in $.ui.keyCode) {
-			specialCodes[$.ui.keyCode[key]] = true;
-		}
-		
-		return function (e) {
-			if (e.which === $.ui.keyCode.ENTER) {
-				if (sulka.grid.getCellEditor() !== null) 
-					sulka.grid.navigateRight();
-				sulka.grid.editActiveCell();
-				sulka.helpers.cancelEvent(e);
-			} else if (!specialCodes.hasOwnProperty(e.which) && sulka.grid.getCellEditor() === null) {
-				// Show editor if user starts typing in a cell
-				sulka.grid.editActiveCell();
-			}
-		};
-	},
-	
-	/**
-	 * Called upon double click. Sets clicked cell editable.
-	 */
-	onDblClick: function() {
-		if (sulka.grid.getCellEditor() === null) {
-			sulka.grid.editActiveCell();
-		}
-	},
-	
-	/**
-	 * Called when drag event is started
-	 */
-	onDragStart: function(e,dd){
-	    var cell = sulka.grid.getCellFromEvent(e);
-	    if (!cell) {
-	    	return;
-	    }
-
-	    dd.row = cell.row;
-	    if (!sulka.getData()[dd.row]) {
-	    	return;
-	    }
-
-	    if (Slick.GlobalEditorLock.isActive()) {
-	    	return;
-	    }
-
-	    e.stopImmediatePropagation();
-	    dd.mode = "recycle";
-
-	    var selectedRows = sulka.grid.getSelectedRows();
-
-	    if (!selectedRows.length || $.inArray(dd.row, selectedRows) == -1) {
-	    	selectedRows = [dd.row];
-	    	sulka.grid.setSelectedRows(selectedRows);
-	    }
-
-	    dd.rows = selectedRows;
-	    dd.count = selectedRows.length;
-
-	    var proxy = $("<span></span>")
-	    	.css({
-	    		position: "absolute",
-	    		display: "inline-block",
-	    		padding: "4px 10px",
-	    		background: "#e0e0e0",
-	    		border: "1px solid gray",
-	    		"z-index": 99999,
-	    		"border-radius": "8px",
-	    		"box-shadow": "2px 2px 6px silver",
-	    		"-moz-border-radius": "8px",
-	    		"-moz-box-shadow": "2px 2px 6px silver",
-	    		"-webkit-border-radius": "8px",
-	    		"-webkit-box-shadow": "2px 2px 6px silver"
-	    	})
-	    	.text("Drag to Recycle Bin to delete " + dd.count + " selected row(s)")
-	    	.appendTo("body");
-
-		dd.helper = proxy;
-
-		$(dd.available).css("background", "pink");
-
-		return proxy;
-		
-	},
-	
-	
-	/**
-	 * onDrag events.
-	 */
-	onDrag: function(e,dd){
-			if (dd.mode != "recycle") {
-				return;
-			}
-			dd.helper.css({top: e.pageY + 5, left: e.pageX + 5});
-	},
-	
-	
-	/**
-	 * onDragEnd: this function is called when dragging rows ends.
-	 */
-	onDragEnd: function(e, dd){
-	    if (dd.mode != "recycle") {
-	    	return;
-	    }
-	    dd.helper.remove();
-	    $(dd.available).css("background", "beige");
-	},
 	
 	
 	/**
@@ -691,20 +446,7 @@ sulka = {
 			$("#header-context-menu").hide();
 		});
 	},
-	    
-    /**
-     * OnBeforeEditCell
-     * 
-     * If row is not an inputRow, deny editing.
-     * 
-     */
-    onBeforeEditCell: function (e, args){
-        if (args.item && args.item.rowStatus === "inputRow") {
-            return true;
-        }
-        return false;
-    },
-	
+
 	/**
 	 * Called when a context menu item is clicked.
 	 */
@@ -1009,80 +751,6 @@ sulka = {
 		return filters;
 	},
 	
-	/**
-	 * When cell is changed, this function is called.
-	 * uses submitSulkaDBRow() to add row to sulka-database
-	 */
-	onCellChange: function(event, args) {
-		if (args.row >= 0) {
-			var row = sulka.getData()[args.row];
-			sulka.submitSulkaDBRow(row);
-		}
-
-		if (sulka.grid.getSelectedRows()[0] === sulka.getData().length - 1){
-			sulka.setData(sulka.getData().concat({rowStatus: "inputRow"}));
-		}
-	},
-	
-	/**
-	 * This function is called when active cell is changed.
-	 * If active row is changed and previous active row was edited, this function validates previous active row.
-	 * 
-	 * Validation adds these fields to the row:
-	 * 	$valid: true, is row is valid, false if not
-	 * 
-	 *  //these are added only if $valid is false
-	 *  $errors: array which contains fields that are invalid. Example: ["species", "municipalities"]
-	 *  $invalid_msg: error msg to be displayed when invalid row is clicked
-	 */
-	onActiveCellChanged: function (e, args) {
-		if (sulka.previousActiveRow !== undefined
-				&& sulka.grid.getSelectedRows()[0] !== sulka.previousActiveRow
-				&& sulka.previousActiveRowEdited ){
-			var data = sulka.getData();
-			var actualRowData = data[sulka.previousActiveRow];
-
-			if (!actualRowData) {
-				return;
-			}
-			
-			sulka.previousActiveRow = sulka.grid.getSelectedRows()[0];
-			
-			sulka.helpers.unsetErrorAndShowLoader();
-			sulka.API.validate(
-				actualRowData,
-				function (data) {
-					sulka.previousActiveRowEdited = false;
-					
-					actualRowData.$valid = data.passes;
-					if (data.passes) {
-						actualRowData.$invalid_msg = undefined;
-						actualRowData.$errors = undefined;
-					} else {
-						actualRowData.$errors = [];
-						errorString = sulka.strings.invalidRow + ": ";
-						for ( var errorField in data.errors)
-							if (data.errors.hasOwnProperty(errorField)) {
-								var errorArray = data.errors[errorField];
-								errorString = errorString.concat('(' + errorField + ': ' + errorArray[0].errorName + '), ');
-								actualRowData.$errors.push(errorField);
-							}
-						actualRowData.$errors = JSON.stringify(actualRowData.$errors);
-						actualRowData.$invalid_msg = errorString;
-					}
-					
-					sulka.submitSulkaDBRow(actualRowData);
-					sulka.helpers.hideLoaderAndUnsetError();
-				}, function() {
-					sulka.helpers.hideLoaderAndSetError();
-				}
-			);
-		}
-		sulka.helpers.showValidationErrors(args);
-		sulka.previousActiveRow = sulka.grid.getSelectedRows()[0];
-		
-	},
-	
 	submitSulkaDBRow: function (row) {
 		if (!row || row.rowStatus !== "inputRow") {
 			// Refuse to submit undefined or read-only row
@@ -1132,7 +800,6 @@ sulka = {
 			sulka.rowsMode = "all";
 		}
 	},
-	
 	
 	/**
 	 * Adds sulka-invalid-cell css class to cells that have been tagged invalid by validation
